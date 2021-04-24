feebd934a8a7acf74432156659cc409751d31423
Abel Cabral de Arruda <abel@ibm.com>
2021-04-24 18:40:28 -0300

    Reparacao Swagger

--- src/main/java/uff/dac/depraia/apidepraia/SwaggerConfig.java
+++ src/main/java/uff/dac/depraia/apidepraia/SwaggerConfig.java
@@ -2,6 +2,8 @@ package uff.dac.depraia.apidepraia;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -10,7 +12,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig {
+public class SwaggerConfig implements WebMvcConfigurer {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
@@ -18,5 +20,11 @@ public class SwaggerConfig {
           .apis(RequestHandlerSelectors.any())
           .paths(PathSelectors.any())
           .build();
+    }    
+    
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
