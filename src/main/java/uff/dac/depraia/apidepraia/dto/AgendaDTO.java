0a9ee2bda4fb88f1dea3f39c6f85757720aff596
Abel Cabral de Arruda <abel@ibm.com>
2021-05-03 22:14:11 -0300

    fix bug

--- src/main/java/uff/dac/depraia/apidepraia/dto/AgendaDTO.java
+++ src/main/java/uff/dac/depraia/apidepraia/dto/AgendaDTO.java
@@ -2,20 +2,23 @@ package uff.dac.depraia.apidepraia.dto;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 import lombok.Getter;
 import uff.dac.depraia.apidepraia.model.Agenda;
 import uff.dac.depraia.apidepraia.model.Praia;
 
 @Getter
-public class AgendaDTO {  
+public class AgendaDTO {    
     private PraiaIdDTO praia;
     private String data;
     private Integer vagas;
 
+    public AgendaDTO() {
+    }
+    
     public AgendaDTO(Agenda agenda) {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         this.praia = new PraiaIdDTO(agenda.getPraia());
-        this.data = agenda.getData().toString();
+        this.data = sdf.format(agenda.getData());
         this.vagas = agenda.getVagas();
     }    
         
