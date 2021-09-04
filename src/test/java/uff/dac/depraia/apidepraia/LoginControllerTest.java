b9aacc5ff1bd48446ab11c315954b330e773ef3f
Abel Cabral de Arruda <abel@ibm.com>
2021-09-04 13:35:06 -0300

    testes de login

--- src/test/java/uff/dac/depraia/apidepraia/LoginControllerTest.java
+++ src/test/java/uff/dac/depraia/apidepraia/LoginControllerTest.java
@@ -1,24 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package uff.dac.depraia.apidepraia;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.net.HttpHeaders;
 import com.google.gson.Gson;
-import com.jayway.jsonpath.JsonPath;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -26,64 +17,78 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import uff.dac.depraia.apidepraia.dto.LoginDTO;
-import uff.dac.depraia.apidepraia.dto.UserDTOSenha;
+import uff.dac.depraia.apidepraia.dto.UserDTO;
 import uff.dac.depraia.apidepraia.model.Endereco;
 import uff.dac.depraia.apidepraia.model.User;
 import uff.dac.depraia.apidepraia.repositories.LoginRepository;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class LoginControllerTest {   
+public class LoginControllerTest {
+
     @Autowired
     private MockMvc mockMvc;
 
     @Autowired
-    private ObjectMapper objectMapper;
-
-    @Autowired
     private LoginRepository loginRepo;
-    
-    Endereco endereco = new Endereco("Rua X", "Bairro Y", "24210201", "Niterói");
-    User user = new User("Abel Cabral Strondemberg", "731.886.080-12", endereco, "email@gmail.com", 2, "killer_queen");        
 
+    private final Endereco endereco = new Endereco("Rua X", "Bairro Y", "24210201", "Niterói");
+    private final User user = new User("Abel Cabral Strondemberg", "731.886.080-12", endereco, "email@gmail.com", 2, "killer_queen");
+    private final Gson gson = new Gson();
+    private final String json = gson.toJson(user);
+    
     @Test
-    void LoginUserTest() throws Exception {
-        LoginDTO entity = new LoginDTO(user);
-        Gson gson = new Gson();
-        String json = gson.toJson(entity);
+    void autenticarTest() throws Exception {
         MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/login")
                 .content(json)
                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.email").value(user.getEmail()))
                 .andReturn();
-        String content = result.getResponse().getContentAsString();
-        int id = JsonPath.read(content, "$.id");
-        deleteUserCreated(id);
-    }
-    
-    @Test
-    void CreateUserTest() throws Exception {
-        UserDTOSenha entity = new UserDTOSenha(user);
-        Gson gson = new Gson();
-        String json = gson.toJson(entity);
-        mockMvc.perform(MockMvcRequestBuilders.post("/cadastro")
-                .content(json)
-                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON))
-                .andDo(print())
-                .andExpect(status().isOk());                   
+        // Como pegar valores do mockMvc!
+        // String content = result.getResponse().getContentAsString();
+        // this.id = JsonPath.read(content, "$.id");
     }
 
     @Test
-    void getUsersTest() throws Exception {
+    void pegarUsuariosTest() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/todos")
                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isOk());
     }
-    
-    private void deleteUserCreated(int id) {
-        loginRepo.deleteById(id);
+
+    @Test
+    void agendasUsuarioTest() throws Exception {
+        Integer id = buscarDados().getBody().getId();
+        mockMvc.perform(MockMvcRequestBuilders.get("/agendasUsuario/{id}", id)
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk());
+        System.out.println(id);
     }
+
+    @Test
+    void criarUsuarioTest() throws Exception {
+        // Busca o id do usuário e deleta;
+        Integer id = buscarDados().getBody().getId();
+        loginRepo.deleteById(id);
+        
+        mockMvc.perform(MockMvcRequestBuilders.post("/cadastro")
+                .content(json)
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+    
+    private ResponseEntity<UserDTO> buscarDados() {        
+        return loginRepo.login(user.getEmail(), user.getSenha())
+                .map(n -> {
+                    return new ResponseEntity<>(new UserDTO(n), HttpStatus.OK);
+                })
+                .orElseGet(() -> {
+                    return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+                });
+    }    
 }
